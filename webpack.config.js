54577bad1a4e1fead57bd85329d910605957635c
virtualnaut <stampcadam@gmail.com>
2023-08-02 21:22:30 +0100

    Preparation for release

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,43 @@
-const path = require("path"),
-  webpack = require("webpack"),
-  HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    app: ["./src/index.ts", "webpack-hot-middleware/client"],
-    vendor: ["react", "react-dom"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].bundle.js",
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  externals: {
+    react: {
+      amd: 'react',
+      commonjs: 'react',
+      commonjs2: 'react',
+    },
+    'react-dom': {
+      amd: 'react-dom',
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+    },
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
       },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    libraryTarget: 'umd',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.d.ts'],
+    modules: ['node_modules'],
   },
   plugins: [
     // new HtmlWebpackPlugin({
     //   template: path.resolve(__dirname, "src", "app", "index.html"),
     // }),
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
   ],
 };
